0bc13edb41ffe631b8bebc03804270c198eaca09
BrahiamTabares <bdtabaresv@uqvirtual.edu.co>
2023-05-27 22:21:33 -0500

    Se agrega la primer parte del ConfigServiceApplication

---
+++ config/src/main/java/co/edu/uniquindio/proyecto/ConfigServiceApplication.java
@@ -0,0 +1,18 @@
+package co.edu.uniquindio.proyecto;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServiceApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceApplication.class, args);
+    }
+
+}
